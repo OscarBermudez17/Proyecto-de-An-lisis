a2f9245e97e85c5100d5fb5a7003a56ebb6798d7
Oscar Bermudez Garcia <obermudezg18@gmail.com>
2016-10-26 18:51:14 -0600

    ejemploPruebaUnitaria

---
+++ PruebasUnitariasPHPUnit/main.php
@@ -0,0 +1,16 @@
+<?php
+class Operacion extends PHPUnit_Framework_TestCase{
+	
+	protected $valor1;
+	protected $valor2;
+	protected $resultado;
+
+		public function testcargar($v){
+		$this->valor1=$v;
+	
+		}
+
+
+}
+
+?>
